bcb7681c69e0aafd12bb78397e71c20a4e49b6a9
Andoni98 <152516872+Andoni98@users.noreply.github.com>
2025-01-08 10:09:48 +0100

    DOM1.js

--- js/DOM1.js
+++ js/DOM1.js
@@ -1,19 +1,12 @@
 const principiante = document.getElementById('principiante');
 const intermedio = document.getElementById('intermedio');
 const avanzado = document.getElementById('avanzado');
-const inicio = document.getElementById('inicio');
 principiante.addEventListener('click', function(){
-    inicio.addEventListener('click', function(){
         window.location.href = 'principiante.html';
-    })
 })
 intermedio.addEventListener('click', function(){
-    inicio.addEventListener('click', function(){
         window.location.href = 'intermedio.html';
-    })
 })
 avanzado.addEventListener('click', function(){
-    inicio.addEventListener('click', function(){
         window.location.href = 'avanzado.html';
-    })
 })
